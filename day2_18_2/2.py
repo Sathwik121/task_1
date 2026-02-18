bfc7121e6936de8dc1a0f71ba51dd5c63c855b40
Sathwik Reddy <125631365+Sathwik121@users.noreply.github.com>
2026-02-18 12:18:43 +0530

    Add script to print the larger of two numbers

---
+++ day2_18_2/2.py
@@ -0,0 +1,9 @@
+#take 2 numbers print the larger one 
+a= int(input(" 1st number"))
+b= int(input(" 2nd number"))
+if a>b:
+  print("the greater number is :", a)
+elif a<b:
+  print("the greater number is :", b)  
+else:
+  print (" both the numbers are equal")  
