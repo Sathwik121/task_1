87bcf9d4b729209ad1686ad86464c70e289cfa4c
Sathwik Reddy <125631365+Sathwik121@users.noreply.github.com>
2026-02-18 12:14:28 +0530

    Implement number positivity check

--- day2_18_2/1.py
+++ day2_18_2/1.py
@@ -1,3 +1,11 @@
-#  write a condition  
-check if a number is positive negative or zero 
+#  check if a number is positive negative or zero 
+
+a = int (input("enter a number"))
+if a < 0 :
+  print ("The number is negative")
+elif a == 0:
+  print ("The number is zero")
+else :
+  print("The number is positive)
+
 
