4a03c779b42ac0807f76985a6e7cfb9f0b8cda4a
Sathwik Reddy <125631365+Sathwik121@users.noreply.github.com>
2026-02-18 12:25:07 +0530

    Create leapyear3.py

---
+++ day2_18_2/leapyear3.py
@@ -0,0 +1,9 @@
+#take a year check whether it is a leap year or not
+
+a = int(input("Enter the year"))
+if (a % 4 == 0) and (a%100== 0) and ( a % 400 == 0):
+  print ( a," is a leap year")
+elif ( a% 4 == 0) and (a % 100 != 0): 
+  print ( a," is a leap year")
+else:
+  print( a,"is not a leap year")   
