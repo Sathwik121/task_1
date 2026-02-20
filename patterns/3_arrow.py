e0e9a49c6b775e129b1adc12fc32abc47566dc85
Sathwik Reddy <125631365+Sathwik121@users.noreply.github.com>
2026-02-20 10:36:49 +0530

    Implement arrow patterns in 3_arrow.py
    Added patterns for side, top, and bottom arrows using asterisks.

--- patterns/3_arrow.py
+++ patterns/3_arrow.py
@@ -1,3 +1,6 @@
+#side arrow
+
+
 for i in range (1,6):
   for j in range (1,i+1):
     print("*",end=" ")
@@ -6,3 +9,28 @@ for i in range (4,0,-1):
   for j in range (1,i+1):
     print("*",end=" ")
   print()
+
+
+for i in range (1,6):
+  print("* "*i)
+for i in range (6,0,-1):
+  print("* "*i)
+
+
+#top arrow
+a = 3
+for i in range (0,a):
+  for j in range (a-i,0,-1):
+    print(" ",end=" ")
+  for k in range (0,2*i+1):
+      print("*",end=" ")
+  print()
+
+#bottom arrow
+a = 3
+for i in range(a, 0, -1):
+    for j in range(a - i, 0, -1):
+        print(" ", end=" ")
+    for k in range(0, 2 * i - 1):
+        print("*", end=" ")
+    print()
