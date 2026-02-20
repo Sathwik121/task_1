15137a888bda0593d7cb7760d3486d9afa78d199
Sathwik Reddy <125631365+Sathwik121@users.noreply.github.com>
2026-02-20 10:34:51 +0530

    Implement right triangle and reverse triangle patterns
    Added patterns for right triangle with numbers and reverse triangle.

--- patterns/1_righttrianglenum.py
+++ patterns/1_righttrianglenum.py
@@ -2,3 +2,16 @@ for i in range (1,6):
   for j in range (1,i+1):
     print(j,end=" ")
   print()
+
+
+for i in range (1,6):
+  for j in range (1,i+1):
+    print(i,end=" ")
+  print()
+
+
+#reverse triangle
+for i in range (5,0,-1):
+  for j in range (1,i+1):
+    print(i,end=" ")
+  print()
